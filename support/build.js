485cebe93ba806074799b64f968d6959b3adfd31
Michel Salib <michelsalib@gmail.com>
2024-08-14 12:26:53 +0200

    Optimize built size

--- support/build.js
+++ support/build.js
@@ -1,11 +1,35 @@
 import { copyFile } from 'copy-file';
 import { globby } from 'globby';
+import { execSync } from 'node:child_process';
 
+console.log('Listing backend deps');
+
+// get backend deps from NPM
+const { dependencies: { backend: { dependencies: backendDependencies } } } = JSON.parse(execSync('npm list -a --json', {
+    encoding: 'utf-8'
+}));
+
+// recurse deps
+function getSubDependencies(deps) {
+    const currentLevel = Object.keys(deps);
+    const lowerLevels = Object.values(deps).filter(dep => dep.dependencies).flatMap(dep => getSubDependencies(dep.dependencies));
+
+    return lowerLevels.reduce((res, cur) => {
+        if (!res.includes(cur)) {
+            res.push(cur);
+        }
+
+        return res;
+    }, currentLevel);
+}
+const flatDependencies = getSubDependencies(backendDependencies);
+
+// time to copy into ./dist
 console.log(`Building dist`);
 
 const pathes = await globby([
     'backend/dist',
-    'node_modules',
+    ...flatDependencies.map(f => `node_modules/${f}/**/*.{js,json}`),
     'backend/package.json',
     'frontend/dist',
     'frontend/package.json',
